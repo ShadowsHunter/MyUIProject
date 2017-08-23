7da90b4c219200470b6a34f8ed25d62e445b48a1
Haochuan <343622685@qq.com>
2017-08-23 09:30:58 +0800

    录音功能初次导入

---
+++ app/src/main/java/hunter/com/myuiproject/fragments/uis/soundrecord/RecordingService.java
@@ -0,0 +1,30 @@
+package hunter.com.myuiproject.fragments.uis.soundrecord;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+
+/**
+ * Description
+ * Created by hcgan on 2017/8/23.
+ */
+
+public class RecordingService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
